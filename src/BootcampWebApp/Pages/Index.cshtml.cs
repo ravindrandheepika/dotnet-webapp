ef6f2d217929066d7f5120ade7248f1d74a14dfb
Dheepika Ravindran <ravindrandheepika@gmail.com>
2026-05-13 22:56:17 -0400

    Restructured project into src/tests and added solution file

---
+++ src/BootcampWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BootcampWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
